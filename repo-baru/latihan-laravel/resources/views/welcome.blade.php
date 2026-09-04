685a10456806b48a59f6fa311fea4abd7b2845bf
nferdy <yogiferdiansyahamtamiluloh@gmail.com>
2026-09-04 21:34:35 +0700

    Upload ulang struktur folder rapi

---
+++ repo-baru/latihan-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Tugas Praktikum Laravel</title>
+</head>
+<body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
+    <h1>Nama: Yogi Ferdiansyah Amta Miluloh</h1>
+    <h2>NIM: H1H024027</h2>
+</body>
+</html>
